f729b61cdba265e3b6d335035a604d10c9ffcfdb
Abdullah <assf23134@icloud.com>
2025-04-29 13:07:44 +0000

    ch4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+{
+	if(a == 'q' || a == 'e')
+		continut;
+
+	putchar(a);
+}
+
+putchar('\n');
+return(0);
+
+}
