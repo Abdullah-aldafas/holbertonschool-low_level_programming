59822d02dbc865c4eff45f7b2b4466bc23c209bc
Abdullah <assf23134@icloud.com>
2025-04-29 13:29:29 +0000

    ch5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	int a;
-	
-		for(a = 0; a <=9; a++)
-		{
-			printf("%d", a);
-		}
-		printf("\n");
-	return (0);
-}
+    int a;
+
+    for (a = 0; a <= 9; a++)
+    {
+        printf("%d", a);
+    }
+
+    printf("\n");
+
+    return (0);
+}`
