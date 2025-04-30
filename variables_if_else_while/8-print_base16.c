5c871196eb142f224f509aafa2a6b446bd38db09
Abdullah <assf23134@icloud.com>
2025-04-30 15:11:53 +0000

    ch8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,21 +5,21 @@
  * Return: Always 0 (Success)
  *
  */
-int main (void)
+int main(void)
 {
 	int d;
 	char x;
 
-	for (d = '0'; d <='9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 	}
-	for (x = 'a'; x <='f'; x++)
+	for (x = 'a'; x <= 'f'; x++)
 	{
 		putchar(x);
 	}
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
