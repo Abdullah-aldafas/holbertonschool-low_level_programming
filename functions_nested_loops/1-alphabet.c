a1097ec7a0f86f01f42d10a7a9f58a729d997041
Abdullah <assf23134@icloud.com>
2025-04-30 17:22:54 +0000

    ch-1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+
+int print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+
+	return(0);
+}
