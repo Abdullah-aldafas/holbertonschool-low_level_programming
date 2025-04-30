e1ba6072fd6d1f57fc56418f8ce22328ac7017ba
Abdullah <assf23134@icloud.com>
2025-04-30 15:23:35 +0000

    ch9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,11 +12,15 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		putchar( i + '0');
-	if (i =!0)
+		putchar( i);
+		if (i =!'0')
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
+	putchar('\n');
+
 		return (0);
 }
 
