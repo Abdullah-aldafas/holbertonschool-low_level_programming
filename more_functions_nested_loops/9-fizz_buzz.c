4fc7450eb6a9a19f120c0288df2faf9327e93258
Abdullah <assf23134@icloud.com>
2025-05-06 13:39:19 +0000

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,6 +6,7 @@
  * Return: Always 0
  */
 
+
 int main(void)
 {
 	int i;
@@ -13,29 +14,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-			printf(" ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-			printf(" ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-			printf(" ");
-		}
-		else if (i == 100)
-		{
-			printf("%d", i);
-		}
 		else
-		{
 			printf("%d", i);
+
+		if (i != 100)
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
