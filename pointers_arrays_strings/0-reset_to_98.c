8aed017b404fbf9e69f034c1bb9c5890c14f39f4
abdullah <assf23134@icloud.com>
2025-05-08 08:26:02 +0000

    0. 98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_line - a function that draws a straight line in the terminal.
- * followed by a new line.
+ * reset_to_98 - a function that takes a pointer to an int as parameter 
+ *  updates the value it points to to 98.
  * @n: An input integer
  * Return: Always 0
  */
