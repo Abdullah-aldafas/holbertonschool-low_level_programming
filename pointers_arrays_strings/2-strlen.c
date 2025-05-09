1b20a687524a5782ec018d84ab87925433349b79
abdullah <assf23134@icloud.com>
2025-05-09 03:24:16 +0000

    2. This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen -  a function that returns the length of a string.
+ * @i: An input integer
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putcha(s[i]);
+	}
+
+	return i;
+}
