f58bcc96d61ac6c82b8638c058cd8564f8cb00b8
Abdullah <assf23134@icloud.com>
2025-05-10 19:40:30 +0000

    ch-2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - a function that copies a string up to n bytes.
+ * _strncpy - copies a string up to n bytes
  * @dest: destination string buffer
  * @src: source string to copy from
  * @n: maximum number of bytes to copy
  *
- * Return: A pointer to the resulting string `dest`
+ * Return: a pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-       
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	
+
 	for (; i < n; i++)
 		dest[i] = '\0';
-	
-	return dest;
+
+	return (dest);
 }
