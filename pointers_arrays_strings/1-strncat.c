b004f57b6f4b32efe1295719c4d0711b5f1ee8b9
Abdullah <assf23134@icloud.com>
2025-05-13 16:59:44 +0000

    swap

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -7,28 +7,26 @@
  * @n: an input integer
  * Return: A pointer to the resulting string
  */
+#include <stdio.h>
+#include "main.h"
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
-
-	while (*src)
-	{
-		srclen++;
-		src++;
-	}
-
-	while (*dest)
-		dest++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	*dest = '\0';
-	return (temp);
+    int i = 0,j = 0;
+    
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+    while( src[j] != '\0' && j < n)
+    {
+         dest[i] = src [j];
+         
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    
+    return dest;
+    
 }
