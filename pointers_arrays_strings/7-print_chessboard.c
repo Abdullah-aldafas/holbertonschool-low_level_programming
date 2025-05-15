2b96510f98ad9fef11fd7b907655abb969cd9d1f
abdullah <assf23134@icloud.com>
2025-05-15 17:21:44 +0000

    ch-7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,17 +8,17 @@
 void print_chessboard(char (*a)[8])
 {
 	int i = 0;
-	
+
 	while (i < 8)
 	{
 		int j = 0;
-		
+
 		while (j < 8)
 		{
-			printf("%c",a[i][j]);
+			_putchar(a[i][j]);
 			j++;
 		}
 		i++
-			printf("\n");
+			 _putchar("\n");
 	}
 }
