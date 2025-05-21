833e82cc0d9a8f719ad884b30d8d08b34f74a6df
Abdullah <assf23134@icloud.com>
2025-05-21 02:45:32 +0000

    ch-2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,16 +11,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	arr = malloc(nmemb * size);
-	char *p;
 
 	if (arr == NULL)
 		return (NULL);
+
 	p = arr;
 
 	for (i = 0; i < nmemb * size; i++)
