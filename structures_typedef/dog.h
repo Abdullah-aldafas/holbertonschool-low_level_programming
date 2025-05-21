f175018b0359b33ab0d7f21715e3600544b63e9d
Abdullah <assf23134@icloud.com>
2025-05-21 22:53:59 +0000

    ch-3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: data structure for dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
