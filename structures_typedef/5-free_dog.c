d807f49a3035d5c918f0409f1b8b90643fa695d9
Abdullah <assf23134@icloud.com>
2025-05-22 14:32:14 +0000

    ch-5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - a function that frees dogs.
+ * @d: An pounter to the dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
