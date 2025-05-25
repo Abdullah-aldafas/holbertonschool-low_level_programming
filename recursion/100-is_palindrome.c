19f6b15c1dacac292187a14aefc1986049e6faf3
Abdullah <assf23134@icloud.com>
2025-05-25 21:33:55 +0000

    ch-100

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * help - Recursively checks if a string is a palindrome
+ * @s: the string
+ * @start: starting index
+ * @end: ending index
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int help(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (help(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: the string to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int end;
+
+	if (*s == '\0')
+		return (1);
+
+	for (end = 0; s[end] != '\0'; end++)
+	{
+	}
+
+	return (help(s, 0, end - 1));
+}
