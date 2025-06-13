49f446da8c3052ade9ce896a49fbf06ea2ea0811
Abdullah <assf23134@icloud.com>
2025-06-13 21:47:35 +0000

    ch-0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@
 size_t print_list(const list_t *h)
 {
 	int i = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		printf("[0] (nil)\n");
 		else
-		printf("[%u] %s\n",h->len, h->str);
-				h = h -> next;
-			       	i++;
+		printf("[%u] %s\n", h->len, h->str);
+				h = h->next;
+				i++;
 	}
 	return (i);
 }
