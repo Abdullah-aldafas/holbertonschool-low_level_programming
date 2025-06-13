244e543bb704b5edcf755d06cb4be14c92257baf
Abdullah <assf23134@icloud.com>
2025-06-13 22:45:32 +0000

    ch-3

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list -  a function that frees a list_t list.
+ * @head: An input pointer
+ * Return: free list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while(head != NULL)
+	{
+		next = head->next;
+		free (head->str);
+		free (head);
+		head = next;
+	}
+}
