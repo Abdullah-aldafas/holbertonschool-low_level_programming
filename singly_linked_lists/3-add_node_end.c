0469fe532a6a6a60d28085d00c2c63056fe5057c
Abdullah <assf23134@icloud.com>
2025-06-13 22:36:02 +0000

    ch-3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * _strlen - a function that count the numer of char
+ * @s: An input pointer
+ * Return: number of char
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * add_node - a function that adds a new node at the beginning of a list_t list
+ * @str: An input pointer
+ * @head: An input pointer to pointer
+ * Return: the new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *add = malloc(sizeof(list_t));
+
+	if (add == NULL)
+		return (NULL);
+
+	add->str = strdup(str);
+	add->len = _strlen(str);
+	add->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = add;
+		return (add);
+	}
+
+	list_t *temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp ->next = add;
+
+	return (add);
+}
