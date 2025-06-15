e20abc1fe7694eae8e10c624a1d4b418c17e2941
Abdullah <assf23134@icloud.com>
2025-06-15 15:16:55 +0000

    ch-4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - a function that frees a dlistint_t list.
+ * @head: An input pounter
+ * Return:  function that frees
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (temp != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+
