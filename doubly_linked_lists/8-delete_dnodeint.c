532337b79ba0a14786292b5b57f1d25f65d5bc54
Abdullah <assf23134@icloud.com>
2025-06-15 17:15:21 +0000

    ch-8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,12 +11,11 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
+	dlistint_t *temp;
+
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-
-	dlistint_t *temp;
 
 	if (index == 0)
 	{
