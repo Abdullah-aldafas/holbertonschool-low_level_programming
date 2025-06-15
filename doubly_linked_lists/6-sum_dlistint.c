f26913f77a2c138386f58850b4870dd019742027
Abdullah <assf23134@icloud.com>
2025-06-15 15:47:29 +0000

    ch-6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - a function that returns the sum of all the data (n)
+ * @head: An input pounter
+ * Return: the sum of all the data (n)
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int result = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head != NULL)
+	{
+		result = head->n;
+		head = head->next;
+	}
+	return (result);
+}
