bec8e1b2b3e9140dcc785509b7fa38a3c0108217
Abdullah <Assf23134@icloud.com>
2025-06-16 14:23:40 +0000

    0-ch

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,17 +17,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-	fd = open (filename, O_RDONLY);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
 
 	buffer = malloc(sizeof(char) * letters);
-
 	if (buffer == NULL)
 	{
 		close(fd);
@@ -35,7 +31,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	h = read(fd, buffer, letters);
-
 	if (h == -1)
 	{
 		free(buffer);
@@ -44,13 +39,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	n = write(STDOUT_FILENO, buffer, h);
-
 	if (n == -1 || n != h)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
+
 	free(buffer);
 	close(fd);
 	return (n);
