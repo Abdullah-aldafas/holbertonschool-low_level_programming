24150c51fae9dc68f73d51dbcab82b15e6ab0be3
Abdullah <Assf23134@icloud.com>
2025-06-16 01:25:52 +0000

    ch-1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary representation of a number
+ * @s: An input pounter
+ * Return:  length of a string.
+ */
+
+void print_binary(unsigned long int n)
+{
+
