0c5292ef194bf02a31ff5ac760bed6533307c49a
Abdullah <Assf23134@icloud.com>
2025-06-16 14:19:17 +0000

    0-ch

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,9 +13,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n  = write(STDOUT_FILENO,buffer,h);
-	char *buffer = malloc(sizeof(char)*letters);
-	ssize_t h = read(fd,buffer,letters);
+	ssize_t n, h;
+	char *buffer;
 
 	if (filename == NULL)
 	{
@@ -27,7 +26,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
+	
+	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
 	{
@@ -35,6 +35,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	h = read(fd, buffer, letters);
 
 	if (h == -1)
 	{
@@ -43,7 +44,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-
+	n = write(STDOUT_FILENO, buffer, h);
+	
 	if (n == -1 || n != h)
 	{
 		free (buffer);
