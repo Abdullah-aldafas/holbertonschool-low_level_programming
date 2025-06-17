cc50ce707f25eaf12f37f250ca409e30fbfb93e7
abdullah <assf23134@icloud.com>
2025-06-17 12:20:37 +0000

    ch-1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,7 +10,7 @@
  * Return: creates a file
  */
 
- int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
 	ssize_t n;
@@ -18,7 +18,7 @@
 	if (filename == NULL)
 		return (-1);
 
-		fd = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -28,7 +28,6 @@
 			len++;
 
 		n = write(fd, text_content, len);
-
 		if (n == -1 || n != len)
 		{
 			close(fd);
